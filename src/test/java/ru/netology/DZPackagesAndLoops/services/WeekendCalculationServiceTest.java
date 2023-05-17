de1c2d9d53935875cc47d5921f541c318ea142ce
Maria <cose@list.ru>
2023-05-17 14:45:21 +0300

    task2

--- src/test/java/ru/netology/DZPackagesAndLoops/services/WeekendCalculationServiceTest.java
+++ src/test/java/ru/netology/DZPackagesAndLoops/services/WeekendCalculationServiceTest.java
@@ -1,16 +1,18 @@
 package ru.netology.DZPackagesAndLoops.services;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 
 public class WeekendCalculationServiceTest {
 
     @ParameterizedTest
-    @CsvSource({
-            "3,10000,3000,20000",
-            "2,100000,60000,150000"
-    })
+    //@CsvSource({
+     //       "3,10000,3000,20000",
+     //       "2,100000,60000,150000"
+    //})
+    @CsvFileSource(files = "src/test/resources/data.csv")
     public void testWeekendCalculation(int expected, int income, int expenses, int threshold) {
         WeekendCalculationService service = new WeekendCalculationService();
 
